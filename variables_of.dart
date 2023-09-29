6751ec38b1901ec278bf7a4d6ed5cd7128fe03dd
ITDawoon <itdawoonkim@gmail.com>
2023-09-30 02:17:49 +0900

    Finished short recap of Dart's variable types

---
+++ variables_of.dart
@@ -0,0 +1,67 @@
+// Basic structure of Dart
+// Dart call and activate only main()
+void main() {
+    // Don't forget to add semicolon at the end
+    print("Hello, world!");
+}
+
+// The var keyword
+void main() {
+  // using var method when the variable is inside the function or method == local variable
+  // Two ways of creating variables: 1.var 2.Using types(String, int, ...)
+  // Variables can be updated till the type is remained
+  var name = "name";
+  String number = "number";
+}
+
+// Dynamic Type
+void main() {
+  // this var can be any types of variables == Dynamic variable
+  dynamic name;
+  name = "yellow monkey";
+  name = 12;
+  name = true;
+}
+
+// Nullable Variables
+void main() {
+  // This "?(Question mark)" meaning that this String can be String/null
+  // Null safety == the specific data/variable may be null
+  // By default, all variables == non-nullable
+  String? name = "name";
+  name = null;
+
+  // First way of null check (these are same func)
+  if (name != null) {
+    name.isNotEmpty;
+  }
+  // Second way of null check (these are same func)
+  name?.isNotEmpty;
+}
+
+// Final Variables
+// final variable can not be changed
+same as "const"
+void main() {
+  final String name = "name";
+
+  // Error occurs when you try to change the var
+  //name = "losAg"
+}
+
+// Late Variables
+// help to create variables without any data
+// declare data LATTER
+// Good for when trying to get the data using API
+void main() {
+  late final String name;
+  name = "name";
+}
+
+// Constant Variables
+// const ONLY use when you know this variable in """complie-time"""
+void main() {
+  const name = 'name';
+  // Error occurs : Cuz const is only using in complie-time
+  //cont API = FetchAPI()
+}
